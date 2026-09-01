ffc3795f0c441683c0c97b4d606e5681b3363ff8
donghoshin07 <donghoshin07@gmail.com>
2026-08-31 23:09:22 -0500

    Update Figure_2_oAdV_Delta-24-RGD_human.R

--- scripts/3_downstream_analyses/Figure_2_oAdV_Delta-24-RGD_human.R
+++ scripts/3_downstream_analyses/Figure_2_oAdV_Delta-24-RGD_human.R
@@ -2,7 +2,7 @@
 # Figures 2 and S2 analyses using Delta-24-RGD data
 # ==============================================================================
 
-# Expects this script to be located in: /scripts
+# Expects this script to be located in: /scripts/3_downstream_analyses
 #
 # Required input files: 
 # ../../NCT00805376_rGBM_oAdV/analysis_output/Global_Atlas_Res0.1.rds
