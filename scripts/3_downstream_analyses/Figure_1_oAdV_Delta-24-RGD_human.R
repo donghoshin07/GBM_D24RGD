9fdf938ba582b2f6170972d6a33e809ae44e67a5
donghoshin07 <donghoshin07@gmail.com>
2026-09-04 11:31:44 -0500

    Update Figure_1_oAdV_Delta-24-RGD_human.R

--- scripts/3_downstream_analyses/Figure_1_oAdV_Delta-24-RGD_human.R
+++ scripts/3_downstream_analyses/Figure_1_oAdV_Delta-24-RGD_human.R
@@ -6,7 +6,7 @@
 #
 # Required input files: 
 # TCR CDR3b sequencing files can be found: GEO: GSE330794
-# Clinical data can be found: Lang et al. JCO, 2018 (PMID:29432077, PMCID:PMC6075856)
+# Clinical data can be found: Lang et al. J Clin Oncol., 2018 (PMID:29432077, PMCID:PMC6075856)
 # ../../input/TCR/patient/*.txt
 #
 # Output directories:
@@ -17,6 +17,8 @@
 # ../../output/TRB_Diversity_N100.rds
 # ../../output/TCR_Diversity_Groups.rds
 # other output files and plots
+#
+# Note: P20 excluded due to clinical consierations (J Clin Oncol., 2018)
 
 if (requireNamespace("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()) {
   try(setwd(dirname(rstudioapi::getActiveDocumentContext()$path)), silent = TRUE)
